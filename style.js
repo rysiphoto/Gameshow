601540fb009fe5b62747bb5c2b41a282c98162fa
Ryan Siverson <rcsskier@mac.com>
2020-03-31 23:22:36 -0600

    completed 5 questions and console logged final score with time penalties for wrong answers

--- style.js
+++ style.js
@@ -1,8 +1,5 @@
 var questionNumber = 0;
 
-
-
-
 function startQuiz() {
     var x = document.getElementById("playGame");
     if (x.style.display === "none") {
@@ -15,7 +12,7 @@ function startQuiz() {
 var timeEl = document.querySelector(".timer");
 var mainEl = document.getElementById("addInits");
 
-var secondsLeft = 5;
+var secondsLeft = 90;
 
 function setTime() {
     var timerInterval = setInterval(function () {
@@ -30,12 +27,13 @@ function setTime() {
     }, 1000);
 }
 function endGame() {
+    console.log(secondsLeft);
     var y = document.getElementById("addInits")
-    if (y.style.display === "none") {
+
+    if (y.style.display == "none") {
         y.style.display = "block";
-    } if (("playGame").display === "block"); {
-        ("gamePlay").display = "none";
     }
+
 };
 
 setTime();
@@ -43,14 +41,11 @@ setTime();
 
 var questions = ["Who is not a member of Led Zeppelin?", "Which song is an AC/DC song?", "Which is a Porno for Pyros song?", "Who was not a singer for Van Halen", "Who was not in Pink Floyd"];
 
-
 var answers1 = ["Robert Palmer", "Jimmy Page", "John Paul Jones", "John Bonham"];
 var answers2 = ["Eruption", "Jungle Love", "Dirty Deeds", "Love Gun"];
 var answers3 = ["Pets", "Been Caught Stealing", "Mountain Song", "Stop"];
 var answers4 = ["Gary Cherone", "Sammy Hagar", "David Lee Roth", "Perry Farrell"];
 var answers5 = ["David Gilmour", "Bob Klose", "Roger Water", "Bob Weir"];
-
-
 
 var answersCorrect = ["Robert Palmer", "Dirty Deeds", "Pets", "Perry Farrell", "Bob Weir"];
 
@@ -61,25 +56,40 @@ var playerTop = document.querySelector("#player-Top");
 var player = [];
 var answersArr = [answers1, answers2, answers3, answers4, answers5];
 init();
-nextQuestion(questions[questionNumber], answersArr[questionNumber]);
+nextQuestion();
 
-function nextQuestion(stQuestion, arrAnswer) {
+function nextQuestion() {
 
-    console.log(stQuestion);
-    document.getElementById("questionBox").innerText = (stQuestion);
-    document.getElementById("button1").innerText = (arrAnswer[0]);
-    document.getElementById("button2").innerText = (arrAnswer[1]);
-    document.getElementById("button3").innerText = (arrAnswer[2]);
-    document.getElementById("button4").innerText = (arrAnswer[3]);
+    // console.log(questions[questionNumber]);
+    console.log(questionNumber);
+    if (questions.length > questionNumber) {
+        document.getElementById("questionBox").innerText = (questions[questionNumber]);
+        document.getElementById("button1").innerText = (answersArr[questionNumber][0]);
+        document.getElementById("button2").innerText = (answersArr[questionNumber][1]);
+        document.getElementById("button3").innerText = (answersArr[questionNumber][2]);
+        document.getElementById("button4").innerText = (answersArr[questionNumber][3]);
+    } else {
+        endGame();
+    }
 };
 
 
-
 function answer(answerThis) {
-    if (onclick.getElementById(answer) = (("Robert Palmer" === "Robert Palmer") || ("Dirty Deeds" === "Dirty Deeds") || ("Pets" === "Pets") || ("Perry Farrell" === "Perry Farrell") || ("Bob Weir" === "Bob Weir"))) {
+
+    if (answersArr[questionNumber][answerThis.value] == answersCorrect[questionNumber]) {
+
+
+        console.log("Correct");
+        questionNumber++;
+
         nextQuestion();
+
     } else {
-        nextQuestion(), secondsLeft - 5;
+        console.log("Wrong Answer");
+        secondsLeft -= 10;
+        questionNumber++;
+        nextQuestion();
+
     };
 };
 
@@ -111,6 +121,12 @@ function init() {
 
     renderPlayer();
 }
+function storeScore() {
+    localStorage.setItem("timer", JSON.stringify(timer));
+}
+
+
+
 
 function storePlayer() {
     localStorage.setItem("player", JSON.stringify(player));
